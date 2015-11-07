d0d5bf7fb8b0f21b70f34a5be49ffcac29c87666
AnMatt <an.mathew822@gmail.com>
2015-11-06 16:03:31 -0800

    Asignmnt-CS565
    Asignmnt-CS565

---
+++ WindowsApplication3/WindowsApplication3/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Number1 = New System.Windows.Forms.TextBox()
+        Me.Number2 = New System.Windows.Forms.TextBox()
+        Me.Sum = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(46, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter first Number"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(49, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(110, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Enter second Number"
+        '
+        'Number1
+        '
+        Me.Number1.Location = New System.Drawing.Point(172, 21)
+        Me.Number1.Name = "Number1"
+        Me.Number1.Size = New System.Drawing.Size(100, 20)
+        Me.Number1.TabIndex = 2
+        '
+        'Number2
+        '
+        Me.Number2.Location = New System.Drawing.Point(172, 69)
+        Me.Number2.Name = "Number2"
+        Me.Number2.Size = New System.Drawing.Size(100, 20)
+        Me.Number2.TabIndex = 3
+        '
+        'Sum
+        '
+        Me.Sum.Location = New System.Drawing.Point(172, 108)
+        Me.Sum.Name = "Sum"
+        Me.Sum.Size = New System.Drawing.Size(100, 20)
+        Me.Sum.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(64, 108)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(28, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Sum"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(83, 185)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Sum)
+        Me.Controls.Add(Me.Number2)
+        Me.Controls.Add(Me.Number1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Number1 As TextBox
+    Friend WithEvents Number2 As TextBox
+    Friend WithEvents Sum As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ WindowsApplication3/WindowsApplication3/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num1, num2 As Integer
+        num1 = Val(Number1.Text)
+        num2 = Val(Number2.Text)
+        Sum.Text = num1 + num2
+    End Sub
+End Class
